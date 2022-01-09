5691b73e71fff670326655ea2718740e2c6a69b8
Marcelo Ferreira <marcelo.ferreira1027@gmail.com>
2022-01-08 21:17:10 -0300

    Terminando Editar contato - controller & DAO

--- src/main/java/br/com/webagenda/controller/Controller.java
+++ src/main/java/br/com/webagenda/controller/Controller.java
@@ -10,10 +10,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.catalina.connector.Response;
+
 import br.com.webagenda.model.DAO;
 import br.com.webagenda.model.JavaBeans;
 
-@WebServlet(urlPatterns = { "/Controller", "/main", "/insert", "/select" })
+@WebServlet(urlPatterns = { "/Controller", "/main", "/insert", "/select", "/update" })
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	DAO dao = new DAO();
@@ -36,6 +38,8 @@ public class Controller extends HttpServlet {
 			novoContato(request, response);
 		} else if (action.equals("/select")) {
 			listarContato(request, response);
+		} else if (action.equals("/update")) {
+			editarContato(request, response);
 		} else {
 			response.sendRedirect("index.html");
 		}
@@ -64,9 +68,9 @@ public class Controller extends HttpServlet {
 	protected void novoContato(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// teste de recebimento dos dados do formulï¿½rio
-		//System.out.println(request.getParameter("nome"));
-		//System.out.println(request.getParameter("fone"));
-		//System.out.println(request.getParameter("email"));
+		// System.out.println(request.getParameter("nome"));
+		// System.out.println(request.getParameter("fone"));
+		// System.out.println(request.getParameter("email"));
 
 		// Setar as variaveis JavaBeans
 		contato.setNome(request.getParameter("nome"));
@@ -80,25 +84,38 @@ public class Controller extends HttpServlet {
 		response.sendRedirect("main");
 
 	}
-	//Editar contato
+
+	// Editar contato =====================================================================/
 	protected void listarContato(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		String idcon = request.getParameter("idcon");
-		//System.out.println(idcon);
-		
+		// System.out.println(idcon);
+
 		// Setar a variavel javaBeans
 		contato.setIdcon(idcon);
 		// Executar o método selecionarContato (DAO)
 		dao.selecionarContato(contato);
-		
-		//Setar os ATRIBUTOS DO FORMULARIO COM OS ATRIBUTOS JAVAbEANS
+
+		// Setar os ATRIBUTOS DO FORMULARIO COM OS ATRIBUTOS JAVAbEANS
 		request.setAttribute("idcon", contato.getIdcon());
 		request.setAttribute("nome", contato.getNome());
 		request.setAttribute("fone", contato.getFone());
 		request.setAttribute("email", contato.getEmail());
-		//Encaminhar ao documento editar.jsp
+		// Encaminhar ao documento editar.jsp
 		RequestDispatcher rd = request.getRequestDispatcher("editar.jsp");
 		rd.forward(request, response);
 	}
-
+	protected void editarContato(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// Setar as variaveis JavaBeans
+		contato.setIdcon(request.getParameter("idcon"));
+		contato.setNome(request.getParameter("nome"));
+		contato.setFone(request.getParameter("fone"));
+		contato.setEmail(request.getParameter("email"));
+		//Executar o método alterarContato
+		dao.alterarContato(contato);
+		// Redirecionar para o documento webAgenda.jsp (Atualizando as alterações nos contatos)
+		response.sendRedirect("main");
+	}
+	
 }
